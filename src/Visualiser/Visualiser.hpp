885fce47ee8efa8ed358047f3d594c6e0a83743b
Thomas Nordentoft <thomas@nordentoft.dk>
2023-04-12 11:04:09 +0100

    Added code to visualise spectrogram

--- src/Visualiser/Visualiser.hpp
+++ src/Visualiser/Visualiser.hpp
@@ -15,15 +15,14 @@ class Visualiser {
     static std::vector<std::pair<std::string, float>> predictions;
     static std::vector<double> fft_data;
     static std::vector<short> envelope_data;
+    static std::vector<std::vector<bool>> spectrogram;
 
     // Keeps predictions vector up-to-date with latest predictions
     static void genre_prediction_callback(const std::vector<std::pair<std::string, float>>& predictions){
         Visualiser::predictions = predictions;
     }
 
-    static void fft_callback(const std::vector<double> & fft_data){
-        Visualiser::fft_data = fft_data;
-    }
+    static void fft_callback(const std::vector<double> & fft_data);
 
     static void envelope_callback(const std::vector<short> & envelope_data){
         Visualiser::envelope_data = envelope_data;
